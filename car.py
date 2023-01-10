785fd0c2d6afde675ac9fb200fad26c8b731b8db
Allison Ogechukwu <ogechukwukanu@gmail.com>
2023-01-10 16:18:29 +0100

    code refactoring to implement new composite

--- car.py
+++ car.py
@@ -1,9 +1,11 @@
-from .interfaces import Serviceable
+from .interfaces import Serviceable, Engine, Battery, Tire
+
 
 class Car(Serviceable):
-    def __init__(self, engine, battery):
-        self.engine = engine
-        self.battery = battery
+    def __init__(self, engine, battery, tire):
+        self.engine: Engine = engine
+        self.battery: Battery = battery
+        self.tire: Tire = tire
 
     def needs_service(self):
-        return self.engine.needs_service() or self.battery.needs_service()
+        return self.engine.needs_service() or self.battery.needs_service() or self.tire.needs_service()
